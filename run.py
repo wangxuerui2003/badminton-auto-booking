540224107074c314c02e066bfe2ea4649ca8e86b
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-05-26 09:51:05 +0800

    feat: app run with different option based on APP_ENV

--- run.py
+++ run.py
@@ -1,6 +1,20 @@
+import os
 from flaskr import create_app
+from flaskr.extensions import db
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    # create all tables for models if not exist
+    with app.app_context():
+        db.create_all()
+
+    if os.environ.get('APP_ENV') == 'local':
+        app.run(debug=True)
+    else:
+        app.run()

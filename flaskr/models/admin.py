a0e5ddf155e2df10898fde29cdaeeca0694e5963
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-05-26 11:30:01 +0800

    fix: remove unnecessary is_admin field from Admin model

--- flaskr/models/admin.py
+++ flaskr/models/admin.py
@@ -8,7 +8,6 @@ class Admin(db.Model, UserMixin):
 	id: Mapped[int] = mapped_column(primary_key=True)
 	username: Mapped[str] = mapped_column(unique=True, nullable=False)
 	password: Mapped[str] = mapped_column(nullable=False)
-	is_admin: Mapped[bool] = mapped_column(default=False)
 
 	def set_password(self, password):
 		self.password = generate_password_hash(password)

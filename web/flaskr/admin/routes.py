5f9b0b798d0b9585929706b5564fd3a7ba572a0f
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-06-03 11:37:40 +0800

    feat: simple user and admin UI

--- web/flaskr/admin/routes.py
+++ web/flaskr/admin/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request
 from flask_login import login_required
 from flaskr.admin import admin_bp
 from .auth import routes
@@ -22,13 +22,14 @@ def index():
     # TODO: display a UI for admin to adjust the desired court time in day, day in week
     return render_template('admin/index.html')
 
-@admin_bp.route('/test-push')
+@admin_bp.route('/new_task', methods=["POST"])
 @login_required
-def test():
-    booking = Booking(date='2024-06-02', time_from=11, time_to=13)
-    db.session.add(booking)
-    db.session.commit()
-    r.rpush(REDIS_QUEUE_KEY, json.dumps(booking.to_dict()))
-    return "nice"
+def new_task():
+    data = request.form
+    # booking = Booking(date='2024-06-02', time_from=11, time_to=13)
+    # db.session.add(booking)
+    # db.session.commit()
+    # r.rpush(REDIS_QUEUE_KEY, json.dumps(booking.to_dict()))
+    return jsonify(data)
 
 
